4031ea0ac5df279cc5b3bdab79d0cb531f34c6fd
Abhishek <abhishekgadad485@gmail.com>
2024-09-26 00:35:05 +0530

    logo rounded

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
     
     <Navbar className='border-b-2'>
        <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-          <span className='px-2 py-1 bg-gradient-to-r from-cyan-600 via-purple-500 to-pink-400 rounded text-white'>Abhishek's</span>
+          <span className='px-2 py-1 bg-gradient-to-r from-cyan-600 via-purple-500 to-pink-400 rounded-lg text-white'>Abhishek's</span>
           Blog
         </Link>
         <form>
