9df8314315a43a66034e0b352a0a2bc849e1f057
Abhishek <abhishekgadad485@gmail.com>
2024-10-07 22:05:27 +0530

    add googleOAuth functionality

--- Client/src/firebase.js
+++ Client/src/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "mern-blog-b90c5.firebaseapp.com",
   projectId: "mern-blog-b90c5",
   storageBucket: "mern-blog-b90c5.appspot.com",
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
             type: String,
             required:true,
          },
+         profilePicture:{
+            type:String,
+            default:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.Yaficbwe3N2MjD2Sg0J9OgHaHa%26pid%3DApi&f=1&,ipt=dc3f76e26120730f7a6514ded141555978e9471240181da8b43485c4b209ec88&ipo=images"
+         }
 }, {timestamps:true}
 );
 
