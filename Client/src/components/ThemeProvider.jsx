98d0187915bf04dc2f7ec0069503dd61fc7df031
Abhishek <abhishekgadad485@gmail.com>
2024-10-08 21:41:44 +0530

    added dark mode functionality

---
+++ Client/src/components/ThemeProvider.jsx
@@ -0,0 +1,13 @@
+import { theme } from 'flowbite-react';
+import React, { Children } from 'react'
+import {useSelector} from 'react-redux';
+
+export default function ThemeProvider() {
+  return (
+    <div className={theme}>   
+        <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-[rgb(16,23,42]">
+          {Children}
+        </div>
+    </div>
+  )
+}
